f9c271cf34d4cec29a1121c0e9c1b0f1b257beda
Coqui2 <francomarcolongo98@gmail.com>
2023-05-18 19:17:14 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 
-
+window.addEventListener('DOMContentLoaded', function() {
+    let elementoPadre = document.getElementById('header');
+    let elementoHijo = document.getElementById('header-plchldr');
+    
+    function actualizarTamanio() {
+      
+      const altoPadre = elementoPadre.offsetHeight;
+    
+      elementoHijo.style.height = altoPadre + 'px';
+    }
+    
+    const resizeObserver = new ResizeObserver(actualizarTamanio);
+    resizeObserver.observe(elementoPadre);
+    
+    actualizarTamanio();
+  });
 
